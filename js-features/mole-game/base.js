16e05e5e0510af9a738b782714cbf1e621327999
Julia Fokanova <oncologmma@mail.ru>
2024-02-26 21:06:05 +0300

    Task 1.3 -> mole-game

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,22 +1,21 @@
+"use strict";
+
 (() => {
-  let playing = true,
-    activeHole = 1;
+  // let playing = true;
+  let activeHole = 1;
 
-  const stop = () => playing = true,
-    getHole = index => document.getElementById(`hole${index}`),
-    deactivateHole = index =>
-      getHole( index ).className = 'hole',
-    activateHole = index =>
-      getHole( index ).className = 'hole hole_has-mole',
-    next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
-      }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1 + Math.random() * 9 );
-      activateHole( activeHole );
-      next();
-    }, 800 );
-
+  // const stop = () => playing = true;
+  const getHole = index => document.getElementById(`hole${index}`);
+  const deactivateHole = index => getHole(index).className = 'hole';
+  const activateHole = index => getHole(index).className = 'hole hole_has-mole';
+  const next = () => setTimeout(() => {
+    // if (!playing) {
+    //   return;
+    // }
+    deactivateHole(activeHole);
+    activeHole = Math.floor(1 + Math.random() * 9);
+    activateHole(activeHole);
+    next();
+  }, 800);
   next();
 })();
