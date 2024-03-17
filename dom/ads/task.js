74e71ac4169bdd5b73d776305e7799a958e817ab
Julia Fokanova <oncologmma@mail.ru>
2024-03-18 02:45:42 +0300

    Task 4.2 -> ads(rotator)

---
+++ dom/ads/task.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const rotators = [...document.getElementsByClassName("rotator")];
+
+function rotate(cases) {
+  let index = cases.findIndex(item => item.classList.contains("rotator__case_active"));
+  let currentCase = cases[index];
+
+  currentCase.classList.remove("rotator__case_active");
+  index = Math.round(Math.random() * (cases.length - 1));
+  currentCase = cases[index];
+  currentCase.classList.add("rotator__case_active");
+
+  if (!currentCase.style.color) {
+    currentCase.style.color = currentCase.dataset.color;
+  }
+  
+  const speed = +currentCase.dataset.speed;
+  setTimeout(rotate, speed, cases);
+}
+
+rotators.forEach(rotator => {
+  const rotatorCases = [...rotator.getElementsByClassName("rotator__case")];
+  // setTimeout(rotate, 1000, rotatorCases);
+  rotate(rotatorCases);
+});
