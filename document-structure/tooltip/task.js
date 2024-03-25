0f03f3726c9d51ab16047c3fae58b5cf1b099aba
Julia Fokanova <oncologmma@mail.ru>
2024-03-26 00:46:57 +0300

    Task 6.1 -> tooltip (bugs fixed)

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -10,23 +10,38 @@ function setTooltipPosition(link, tooltip, dataPosition) {
   const { top, bottom, left, right } = link.getBoundingClientRect();
   switch(dataPosition) {
     case "top": {
-      tooltip.style.top = `${window.scrollY + top - tooltip.offsetHeight}px`;
+      if (tooltip.offsetHeight > top) {
+        tooltip.style.top = `${window.scrollY + bottom}px`;
+      } else {
+        tooltip.style.top = `${window.scrollY + top - tooltip.offsetHeight}px`;
+      }
       tooltip.style.left = `${window.scrollX + left}px`;
       break;
     }
     case "bottom": {
-      tooltip.style.top = `${window.scrollY + bottom}px`;
+      if (document.documentElement.clientHeight - bottom < tooltip.offsetHeight) {
+        tooltip.style.top = `${window.scrollY + top - tooltip.offsetHeight}px`;
+      } else {
+        tooltip.style.top = `${window.scrollY + bottom}px`;
+      }
       tooltip.style.left = `${window.scrollX + left}px`;
       break;
     }
     case "left": {
       tooltip.style.top = `${window.scrollY + top - Math.abs(tooltip.offsetHeight - link.offsetHeight) / 2}px`; 
       tooltip.style.left = `${window.scrollX + left - tooltip.clientWidth}px`; 
+      if (parseInt(tooltip.style.left) < 0) {
+        tooltip.style.left = `${window.scrollX + right}px`;
+      }
       break;
     }
     case "right": {
       tooltip.style.top = `${window.scrollY + top - Math.abs(tooltip.offsetHeight - link.offsetHeight) / 2}px`;
-      tooltip.style.left = `${window.scrollX + right}px`;
+      if (document.documentElement.clientWidth - right < tooltip.offsetWidth) {
+        tooltip.style.left = `${left - tooltip.offsetWidth}px`;
+      } else {
+        tooltip.style.left = `${window.scrollX + right}px`;
+      }
     }
   }
 }
@@ -37,12 +52,14 @@ links.forEach(link => {
 
     if (link.nextElementSibling === tooltip) {
       tooltip.classList.toggle("tooltip_active");
-      return;
-    } 
-    
-    tooltip.innerText = link.title;
-    tooltip.classList.add("tooltip_active");
-    link.after(tooltip);
-    setTooltipPosition(link, tooltip, tooltip.dataset.position);
+    } else {
+      tooltip.innerText = link.title;
+      tooltip.classList.add("tooltip_active");
+      link.after(tooltip);
+    }
+
+    if (tooltip.classList.contains("tooltip_active")) {
+      setTooltipPosition(link, tooltip, tooltip.dataset.position);
+    }
   })
 });
